4d6c125b873194279959108925f1b578187b3d4d
miyoshi <twinz344@gmail.com>
2017-10-17 15:13:30 +0900

    Musicとりあえず追加

--- Program/Music.cs
+++ Program/Music.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1.Program
-{
-    class Music
+class Music
+{ 
+    public async Task JoinCanncel(IVoiceCannnel cannel = null)
     {
+        cannel = cannel ?? (msg.Author as IGuildUser)?.VoiceChanncel;
+        if(cannel == null) { await msg.Cannel.SendMassageAsync(""); return; }
+    }
+
+    private var audioClient = await cannel.ConnectAsync();
+
+    private Process CreateStream(string path)
+    {
+        var ffmpeg = new PocessStarInfo
+        {
+            FileName = "ffmpeg",
+            Arguments = $" -i {path} -ac 2 -f s16le -ar 48000 pipe:1",
+            UseShellxecute = false,
+            RedirectStandardOutput = true,
+        };
+
+        return Process.Start(ffmpeg);
+    }
+
+    private async Task SendAync(IAudioClient client, string path)
+    {
+        var ffmpeg = CreateStream(path);
+        var output = ffmpeg.StandardOutput.BaseStream;
+        var discord = client.CreatePCMStream(AudioApplication.Mixed);
+        await output.CopyToAync(discord);
+        await discord.FlushAsync();
     }
 }
